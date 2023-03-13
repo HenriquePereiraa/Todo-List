f6b34b64af78ccd21c1caa26e65016e87d1fc2de
Henrique Pereira <henrique802011@gmail.com>
2023-03-13 20:30:06 -0300

    todo - button icon added

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,11 +1,12 @@
 import styles from './Main.module.css'
+import { PlusCircle } from 'phosphor-react'
 
 export function Main() {
   return (
     <div className={styles.main}>
         <form className={styles.mainForm}>
             <input type="text" placeholder='Adicione uma nova tarefa' />
-            <button>Criar</button>
+            <button>Criar <PlusCircle size={20}/> </button>
         </form>
     </div>
   )
