944cc9689019eefd4a4544d504efffb773e6d76d
Henrique Pereira <henrique802011@gmail.com>
2023-03-13 22:08:04 -0300

    todo - adde responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
+import styles from "./App.module.css";
 import "./global.css";
 
 export function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Main/>
+    <div className={styles.App}>
+      <Header />
+      <Main />
     </div>
   );
 }
