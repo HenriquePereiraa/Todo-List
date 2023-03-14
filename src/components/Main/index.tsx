23940075013d3af23aa60e74dcc5a5da1f457d6d
Henrique Pereira <henrique802011@gmail.com>
2023-03-13 21:02:43 -0300

    todo - design for empty list made

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,13 +1,36 @@
-import styles from './Main.module.css'
-import { PlusCircle } from 'phosphor-react'
+import styles from "./Main.module.css";
+import { ClipboardText, PlusCircle } from "phosphor-react";
 
 export function Main() {
   return (
     <div className={styles.main}>
-        <form className={styles.mainForm}>
-            <input type="text" placeholder='Adicione uma nova tarefa' />
-            <button>Criar <PlusCircle size={20}/> </button>
-        </form>
+      <form className={styles.mainForm}>
+        <input type="text" placeholder="Adicione uma nova tarefa" />
+        <button>
+          Criar <PlusCircle size={20} />{" "}
+        </button>
+      </form>
+
+      <div className={styles.todoList}>
+        <div className={styles.todoListHeader}>
+          <div className={styles.todoInfos}>
+            <strong className={styles.blue}>Tarefas criadas</strong>
+            <span>0</span>
+          </div>
+          <div className={styles.todoInfos}>
+            <strong className={styles.purple}>Concluídas</strong>
+            <span>0</span>
+          </div>
+        </div>
+
+        <div className={styles.line}></div>
+
+        <div className={styles.emptyList}>
+          <ClipboardText size={56} />
+          <strong>Você ainda não tem tarefas cadastradas </strong>
+          <span> Crie tarefas e organize seus itens a fazer</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
